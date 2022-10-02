a53abe71b1c810331541ef7079517539d7874f90
tlsalfla96 <tlsalfla96@naver.com>
2022-10-03 00:00:33 +0900

    서버정상실행확인

--- src/main/java/com/thehandsome/app/controller/HomeController.java
+++ src/main/java/com/thehandsome/app/controller/HomeController.java
@@ -1,26 +1,18 @@
 package com.thehandsome.app.controller;
 
-import java.util.List;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.thehandsome.app.dto.BrandDTO;
-import com.thehandsome.app.dto.CategoryDTO;
-import com.thehandsome.app.service.BrandService;
-import com.thehandsome.app.service.CategoryService;
+import lombok.extern.log4j.Log4j;
 
 @Controller
+@RequestMapping("/")
+@Log4j
 public class HomeController {
-
+	@RequestMapping(value="", method= {RequestMethod.GET,RequestMethod.POST})
+	public void basic() {
+		log.info("실행");
+	}
 	
 }
