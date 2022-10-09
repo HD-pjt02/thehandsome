9c33cb6431bdab8f75ab86a0f640f7ec52a671a6
tlsalfla96 <tlsalfla96@naver.com>
2022-10-09 13:50:06 +0900

    소스반영

--- src/main/java/com/thehandsome/app/service/impl/MemberServiceImpl.java
+++ src/main/java/com/thehandsome/app/service/impl/MemberServiceImpl.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import com.thehandsome.app.mapper.MemberMapper;
 import com.thehandsome.app.service.MemberService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class MemberServiceImpl implements MemberService{
 	
